8f951cb9b32f86e85fca7dee7007a79ca4222691
iMayhem <sujeetunbeatable@gmail.com>
2026-06-22 15:50:34 +0000

    Fix mobile Watch Together unmount/remount loop on room creation by returning path as route key

--- mobile-site/src/App.vue
+++ mobile-site/src/App.vue
@@ -21,9 +21,6 @@ const getRouteKey = (route: RouteLocationNormalizedLoaded) => {
     if (route.name === 'AnimeDetail') {
         return 'anime-detail';
     }
-    if (route.name === 'Party') {
-        return route.fullPath;
-    }
     return route.path;
 };
 

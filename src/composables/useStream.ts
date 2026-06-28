77c27c5741f34ea008bce85daa0d04c6e5ea799e
iMayhem <sujeetunbeatable@gmail.com>
2026-06-28 04:40:04 +0000

    add Motichoor Ladoo (vidsuper) as server #16

--- src/composables/useStream.ts
+++ src/composables/useStream.ts
@@ -65,7 +65,8 @@ export const movieServers = ref<Server[]>([
   { name: 'Kulfi', urlTemplate: 'https://player.autoembed.app/embed/movie/{tmdbId}' },
   { name: 'Mysore Pak', urlTemplate: 'https://vidfast.pro/movie/{tmdbId}' },
   { name: 'Imarti', urlTemplate: 'https://111movies.com/movie/{tmdbId}' },
-  { name: 'Ghevar', urlTemplate: 'https://vidora.su/movie/{tmdbId}?parameters' }
+  { name: 'Ghevar', urlTemplate: 'https://vidora.su/movie/{tmdbId}?parameters' },
+  { name: 'Motichoor Ladoo', urlTemplate: 'https://vidsuper.net/movie/{tmdbId}' }
 ]);
 
 export const tvServers = ref<Server[]>([
@@ -84,7 +85,8 @@ export const tvServers = ref<Server[]>([
   { name: 'Kulfi', urlTemplate: 'https://player.autoembed.app/embed/tv/{externalId}/{season}/{episode}' },
   { name: 'Mysore Pak', urlTemplate: 'https://vidfast.pro/tv/{externalId}/{season}/{episode}' },
   { name: 'Imarti', urlTemplate: 'https://111movies.com/tv/{externalId}/{season}/{episode}' },
-  { name: 'Ghevar', urlTemplate: 'https://vidora.su/tv/{externalId}/{season}/{episode}?autoplay=true' }
+  { name: 'Ghevar', urlTemplate: 'https://vidora.su/tv/{externalId}/{season}/{episode}?autoplay=true' },
+  { name: 'Motichoor Ladoo', urlTemplate: 'https://vidsuper.net/tv/{externalId}/{season}/{episode}' }
 ]);
 
 const idToNameMap: Record<string, string> = {
@@ -103,7 +105,8 @@ const idToNameMap: Record<string, string> = {
   autoembed: 'Kulfi',
   vidfast: 'Mysore Pak',
   movies111: 'Imarti',
-  vidora: 'Ghevar'
+  vidora: 'Ghevar',
+  vidsuper: 'Motichoor Ladoo'
 };
 
 export const isDefaultServerLoaded = ref(false);

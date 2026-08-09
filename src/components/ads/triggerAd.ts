4f004175c854901fecc1422ba58da31ef270894f
iMayhem <sujeetunbeatable@gmail.com>
2026-08-09 06:57:20 +0000

    fix(ads): replace runtime ad loader (triggerAd) with new tag, drop legacy Admaven comment

--- src/components/ads/triggerAd.ts
+++ src/components/ads/triggerAd.ts
@@ -1,14 +1,10 @@
-const AD_DOMAIN = 'dc9xwpjprguup.cloudfront.net';
+const AD_DOMAIN = '//fa.forsungxylols.com/rn57m0jviM5eK/147918';
 
 export function triggerAd() {
     const s = document.createElement('script');
     s.setAttribute('data-cfasync', 'false');
-    s.src = `https://${AD_DOMAIN}/?pwxcd=1436467`;
+    s.async = true;
+    s.type = 'text/javascript';
+    s.src = AD_DOMAIN;
     document.head.appendChild(s);
-    setTimeout(() => {
-        const s2 = document.createElement('script');
-        s2.setAttribute('data-cfasync', 'false');
-        s2.src = `https://${AD_DOMAIN}/?pwxcd=1448933`;
-        document.head.appendChild(s2);
-    }, 100);
 }
